d9b06f764382508c0a3b79bd978787f0ffb38ea9
Sergey <sv0lapin@gmail.com>
2014-03-26 17:09:24 +0400

    Added speed labels

--- js/Rectangle.js
+++ js/Rectangle.js
@@ -1,123 +1,124 @@
 /**
-*	Creates a new Rectangle object:
-*	@constructor
-*	@param {HTMLCanvasElement} canvas A reference to a canvas element
+* Creates a new Rectangle object:
+* @constructor
+* @param {HTMLCanvasElement} canvas A reference to a canvas element
+* @param {Function} afterSpeedChange A function called each time speed was modified
 */
-function Rectangle(canvas) {
-	// check if canvas context is available:
-	if( !(canvas && canvas.getContext) ) {
-		throw new Error('Canvas context is unavailable');
-	}
-	
-	// saving references to canvas and is context:
-	this.canvas = canvas;
-	this.ctx = canvas.getContext('2d');
-	
-	// setting fill color to black:
-	this.ctx.fillStyle = "#000";
-	
-	// initializinig coordinates and speed:
-	this.speedX = 0;
-	this.speedY = 0;
-	this.X = 0;
-	this.Y = 0;
-	
-	// rectangle size:
-	this.size = 50;
-	
-	// initializing animation timer ID:
-	this.timerID = null;
-	
-	// saving reference to this (Rectangle instance) for
-	// referencing in the event handler:
-	var self = this;
-	
-	document.addEventListener('keydown', function(e) {
-		switch(e.keyCode) {
-			case 38:
-				// down key was pressed:
-				self.speedY -= 1;
-				break;
-			case 40:
-				// up key was pressed:
-				self.speedY += 1;
-				break;
-			case 37:
-				// left key was pressed:
-				self.speedX -= 1;
-				break;
-			case 39:
-				// right key was pressed:
-				self.speedX += 1;
-				break;
-		}
-	}, false);
+function Rectangle(canvas, afterSpeedChange) {
+  // check if canvas context is available:
+  if( !(canvas && canvas.getContext) ) {
+    throw new Error('Canvas context is unavailable');
+  }
+  
+  // saving references to canvas and is context:
+  this._canvas = canvas;
+  this._ctx = canvas.getContext('2d');
+
+  // register callback:
+  if(typeof afterSpeedChange === 'function')
+    this.afterSpeedChange = afterSpeedChange;
+  
+  // initializinig coordinates and speed:
+  this._speedX = 0;
+  this._speedY = 0;
+  this._X = 0;
+  this._Y = 0;
+  
+  // rectangle size:
+  this._size = 50;
+  
+  // initializing animation timer ID:
+  this.timerID = null;
+
+  // setting fill color to black:
+  this._ctx.fillStyle = "#000";
+
 };
 
 /**
-*	Sets initial positiona and speed and begins animation loop
+* Sets initial positiona and speed and begins animation loop
 */
 Rectangle.prototype.start = function() {
-	// saving reference to this for
-	// referencing in requestAnimFrame callback:
-	var self = this;
-	
-	// initial speed and zero:
-	this.speedX = 0;
-	this.speedY = 0;
-	// initial coordinates are at the canvas' center:
-	this.X = parseInt( (this.canvas.width - this.size) / 2, 10 );
-	this.Y = parseInt( (this.canvas.height - this.size) / 2, 10 );
+  // saving reference to this for
+  // referencing in requestAnimFrame callback:
+  var self = this;
+  
+  // initial speed and zero:
+  this._speedX = 0;
+  this._speedY = 0;
+  this.afterSpeedChange && this.afterSpeedChange.call(this);
+  // initial coordinates are at the canvas' center:
+  this._X = parseInt( (this._canvas.width - this._size) / 2, 10 );
+  this._Y = parseInt( (this._canvas.height - this._size) / 2, 10 );
 
-	// just a shorthand:
-	var ctx = this.ctx;
-	
-	(function animationLoop(){
-		self.timerID = requestAnimFrame(animationLoop);
-		self.clearCanvas();
-		
-		// planned coordinate at the next step:
-		var nextX = self.X + self.speedX;
-		var nextY = self.Y + self.speedY;
-		
-		// flags to set true on the canvas edges:
-		var isLeftEgdeReached = (nextX < 0) && (self.speedX < 0);
-		var isRightEgdeReached = ( (nextX + self.size) > self.canvas.width );
-		var isTopEdgeReached = (nextY < 0) && (self.speedY < 0);
-		var isBottomEdgeReached = ( (nextY + self.size) > self.canvas.height );
-		
-		if( isLeftEgdeReached || isRightEgdeReached ) {
-			// reflect from vertical edge:
-			self.speedX = -self.speedX;
-		}
-		
-		if( isTopEdgeReached || isBottomEdgeReached ) {
-			// reflect from horizontal edge:
-			self.speedY = -self.speedY;
-		}
-		
-		// set new coordinates:
-		self.X += self.speedX;
-		self.Y += self.speedY;
-		
-		ctx.fillRect(self.X, self.Y, self.size, self.size);
-	})();
+  // just a shorthand:
+  var ctx = this._ctx;
+  
+  (function animationLoop(){
+    self.timerID = requestAnimFrame(animationLoop);
+    self.clearCanvas();
+    
+    // planned coordinate at the next step:
+    var nextX = self._X + self._speedX;
+    var nextY = self._Y + self._speedY;
+    
+    // flags to set true on the canvas edges:
+    var isLeftEgdeReached = (nextX < 0) && (self._speedX < 0);
+    var isRightEgdeReached = ( (nextX + self._size) > self._canvas.width );
+    var isTopEdgeReached = (nextY < 0) && (self._speedY < 0);
+    var isBottomEdgeReached = ( (nextY + self._size) > self._canvas.height );
+    
+    if( isLeftEgdeReached || isRightEgdeReached ) {
+      // reflect from vertical edge:
+      self._speedX = -self._speedX;
+      self.afterSpeedChange && self.afterSpeedChange.call(self);
+    }
+    
+    if( isTopEdgeReached || isBottomEdgeReached ) {
+      // reflect from horizontal edge:
+      self._speedY = -self._speedY;
+      self.afterSpeedChange && self.afterSpeedChange.call(self);
+    }
+    
+    // set new coordinates:
+    self._X += self._speedX;
+    self._Y += self._speedY;
+    
+    ctx.fillRect(self._X, self._Y, self._size, self._size);
+  })();
+};
+
+Rectangle.prototype.modifyXSpeed = function(delta){
+  this._speedX += delta;
+  this.afterSpeedChange && this.afterSpeedChange.call(this);
+};
+
+Rectangle.prototype.modifyYSpeed = function(delta){
+  this._speedY += delta;
+  this.afterSpeedChange && this.afterSpeedChange.call(this);
 };
 
 Rectangle.prototype.stop = function() {
-	// stopping animation:
-	cancelAnimFrame(this.timerID);
-	this.timerID = null;
-	
-	this.clearCanvas();
+  // stopping animation:
+  cancelAnimFrame(this.timerID);
+  this.timerID = null;
+  
+  this.clearCanvas();
 };
 
-
 Rectangle.prototype.clearCanvas = function() {
-	this.ctx.save();
-	// Use the identity matrix while clearing the canvas
-	this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	// Restore the transform
-	this.ctx.restore();
+  this._ctx.save();
+  // Use the identity matrix while clearing the canvas
+  this._ctx.setTransform(1, 0, 0, 1, 0, 0);
+  this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+  // Restore the transform
+  this._ctx.restore();
+};
+
+Rectangle.prototype.getSpeedX = function(){
+  return this._speedX;
+};
+
+Rectangle.prototype.getSpeedY = function(){
+  return this._speedY;
 };
--- js/app.js
+++ js/app.js
@@ -23,18 +23,53 @@ function init() {
   var canvas = document.getElementById('canvas');
   var hint = document.getElementById('hint');
 
-  window.rect = new Rectangle(canvas);
+  var elSpeedLabels = document.getElementById('speed-labels');
+  var elSpeedX = document.getElementById('speedx');
+  var elSpeedY = document.getElementById('speedy');
+
+  window.rect = new Rectangle(canvas, refreshSpeedLabels);
+
+  function refreshSpeedLabels(){
+    elSpeedX.textContent = this.getSpeedX();
+    elSpeedY.textContent = this.getSpeedY();
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    switch(e.keyCode) {
+      case 38:
+        // down key was pressed:
+        rect.modifyYSpeed(-1);
+        break;
+      case 40:
+        // up key was pressed:
+        rect.modifyYSpeed(1);
+        break;
+      case 37:
+        // left key was pressed:
+        rect.modifyXSpeed(-1);
+        break;
+      case 39:
+        // right key was pressed:
+        rect.modifyXSpeed(+1);
+        break;
+    }
+  }, false);
+
 
   document.getElementById('btn-start-stop').addEventListener('click', function(){
     if(!rect.timerID) {
       rect.start();
       hint.textContent = "Use arrow keys to change horizontal and vertical speed";
       this.textContent = 'Stop animating';
+
+      elSpeedLabels.style.display = 'block';
     }
     else {
       rect.stop();
       this.textContent = 'Start animating';
       hint.textContent = "Click \"Start animating\" button";
+
+      elSpeedLabels.style.display = 'none';
     }
   }, false);
 }
